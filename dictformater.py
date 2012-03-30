77615e61b761e001c2d6f971f04310eba51eb2c0
Erik Smartt <code@eriksmartt.com>
2012-03-30 15:14:43 -0500

    adding csvtodict, for those non-json needs

--- dictformater.py
+++ dictformater.py
@@ -1,3 +1,4 @@
+import csv
 
 
 def reformat(d, clean_func=None, clean_func_dict=None, verbose=False):
@@ -34,3 +35,34 @@ def reformat(d, clean_func=None, clean_func_dict=None, verbose=False):
                             print("reformat(): ERROR: {e}".format(e=e))
 
     return d
+
+
+def csvtodict(filename, clean_func=None, clean_func_dict=None, verbose=False):
+    """
+    @param    filename           String                            File name to open()
+
+    @param    clean_func         Function pointer                  Optional. If provided, each 'cell' will be processed
+                                                                   by this function.
+
+    @param    clean_func_dict    Dictionary of function pointer    Optional. Takes the form of {header: function_pointer},
+                                                                   where each 'cell' in the CSV will be used to lookup a
+                                                                   function for processing based on said cell's header.
+
+    @param    verbose            Boolean                           Used to print() debug info.
+    """
+    with open(filename, 'r') as fp:
+        reader = csv.DictReader(fp)
+
+        c = 0
+
+        for row in reader:
+            if verbose:
+                print("csvtojson('{filename}'): Processing line: {l}".format(filename=filename, l=row))
+
+            if c:  # We're using 'c' as a flag for skipping the first row
+                row = reformat(row, clean_func=clean_func, clean_func_dict=clean_func_dict, verbose=verbose)
+
+            else:
+                c = 1
+
+            yield row
